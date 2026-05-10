fcf20b48d9cad6f5c068aeb7dde8fdeb759a1eef
Harshita Jain <harshitajain2612@gmail.com>
2026-05-10 18:20:18 +0530

    Update CustomTTAReporter path - moved to root directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: [
     ['html'],
     ['allure-playwright'],
-    ['./tests/utils/CustomTTAReporter.ts']
+    ['./CustomTTAReporter.ts']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
